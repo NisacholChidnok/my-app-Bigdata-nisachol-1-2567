3939b1b11c4511c3dbf22b418b3b9e86c7793562
“pranpriya61” <saintpranpriya@gmail.com>
2024-07-26 12:45:13 +0700

    frist commit

--- app/chatgpt/page.tsx
+++ app/chatgpt/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import About from '../components/About'
+import ChatGPT from '../components/ChatGPT'
 
 export default function page() {
   return (
     <div>
-    <About/>
+      <ChatGPT/>
     </div>
   )
 }
